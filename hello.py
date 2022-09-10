aa35deb70d8e6b9c58ca99281b715a27266a9610
Matheus Waked Franca <matheus363@hotmail.com>
2022-09-10 02:42:19 -0300

    Fixed chatterbot file problem

--- hello.py
+++ hello.py
@@ -1,6 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
 app = Flask(__name__)
+
+english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+
+trainer = ListTrainer(english_bot)
+
+trainer.train([
+    "Hello",
+    "Hello, I am fine and how about you?"
+])
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get("msg")
+    return str(english_bot.get_response(userText))
 
 @app.route("/hello")
 
